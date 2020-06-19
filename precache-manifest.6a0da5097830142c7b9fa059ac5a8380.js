7acc05fc8afbe2a42debaa014f475bfef578abdf
Ciorap88 <vlad.014916@gmail.com>
2020-06-19 16:51:57 +0300

    Updates

---
+++ precache-manifest.6a0da5097830142c7b9fa059ac5a8380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cbf98245470f664117e3eeb6cd862e",
+    "url": "/Sudoku-Solver/index.html"
+  },
+  {
+    "revision": "f83d25b4d7a5c4921b87",
+    "url": "/Sudoku-Solver/static/css/main.259de986.chunk.css"
+  },
+  {
+    "revision": "c988be97c69a80b4c3cb",
+    "url": "/Sudoku-Solver/static/js/2.a753b939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sudoku-Solver/static/js/2.a753b939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83d25b4d7a5c4921b87",
+    "url": "/Sudoku-Solver/static/js/main.b8d80aff.chunk.js"
+  },
+  {
+    "revision": "af79cbba52dcafb43886",
+    "url": "/Sudoku-Solver/static/js/runtime-main.20957e55.js"
+  }
+]);
